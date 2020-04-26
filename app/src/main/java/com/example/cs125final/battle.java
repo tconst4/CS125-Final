de9f5edbd13786d3248714858319486df57d74e7
Nate <Gat3k33p3r!>
2020-04-25 23:48:13 -0500

    populate battle and moveList

--- app/src/main/java/com/example/cs125final/battle.java
+++ app/src/main/java/com/example/cs125final/battle.java
@@ -1,4 +1,26 @@
 package com.example.cs125final;
 
+import java.util.Random;
+
 public class battle {
+    private int  move;
+    private static int battleCounter;
+
+    public void setMove() {
+        Random rand = new Random();
+        int newInt = rand.nextInt(3);
+        move = newInt;
+    }
+
+    public int getMove() {
+        return move;
+    }
+
+    public int getEnemy() {
+        return (battleCounter);
+    }
+
+    public void advanceRound() {
+        battleCounter++;
+    }
 }
--- app/src/main/java/com/example/cs125final/moveList.java
+++ app/src/main/java/com/example/cs125final/moveList.java
@@ -1,4 +1,31 @@
 package com.example.cs125final;
 
 public class moveList {
+
+    private String[][] moves = new String[6][3];;
+
+    public void create() {
+        moves[0][0] = " ";
+        moves[0][1] = " ";
+        moves[0][2] = " ";
+        moves[1][0] = " ";
+        moves[1][1] = " ";
+        moves[1][2] = " ";
+        moves[2][0] = " ";
+        moves[2][1] = " ";
+        moves[2][2] = " ";
+        moves[3][0] = " ";
+        moves[3][1] = " ";
+        moves[3][2] = " ";
+        moves[4][0] = " ";
+        moves[4][1] = " ";
+        moves[4][2] = " ";
+        moves[5][0] = " ";
+        moves[5][1] = " ";
+        moves[5][2] = " ";
+    }
+
+    public String getMove(int battle, int move) {
+        return moves[battle][move];
+    }
 }

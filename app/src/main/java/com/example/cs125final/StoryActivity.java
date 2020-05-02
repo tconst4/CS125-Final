dba95530ebb2c04aecb552428befa743802055bd
tconst4 <tconst4@illinois.edu>
2020-05-02 18:03:33 -0500

    button working

--- app/src/main/java/com/example/cs125final/StoryActivity.java
+++ app/src/main/java/com/example/cs125final/StoryActivity.java
@@ -2,7 +2,9 @@ package com.example.cs125final;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class StoryActivity extends AppCompatActivity {
@@ -12,5 +14,13 @@ public class StoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
+        Intent toGame = new Intent(StoryActivity.this, TransitionActivity.class);
+        startGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(toGame);
+            }
+        });
+
     }
 }

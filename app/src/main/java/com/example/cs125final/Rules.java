90935f70ce5b4bc46a9e8c35c6cf48654df4f066
tconst4 <tconst4@illinois.edu>
2020-05-04 12:02:52 -0500

    x

--- app/src/main/java/com/example/cs125final/Rules.java
+++ app/src/main/java/com/example/cs125final/Rules.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class GeoffRound extends Battle {
+public class Rules extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_geoff_round);
+        setContentView(R.layout.activity_rules);
     }
 }
